9edca86bf09534a7480a69d76e1ec8b0ff859d4d
Dominik Ludera <domludera@gmail.com>
2020-04-24 15:47:28 -0400

    aframe integration

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'aframe';
-import 'aframe-textarea-component';
 import App from './js/App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
